7e565c752f479dc081264a5094fff09fbe155533
Furkan İlbahar <furkan_ilbahar@hotmail.com>
2016-08-18 03:59:38 +0300

    Alert part is not working

---
+++ MySimpleMvcProject/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySimpleMvcProject.Models
+{
+    public class Student
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Surname
+        {
+            get;
+            set;
+        }
+
+        public DateTime RegisterDate
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MySimpleMvcProject/Models/StudentList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySimpleMvcProject.Models
+{
+    public class StudentList
+    {
+        public static List<Student> studentList = new List<Student>();
+        public static bool isDeleted;
+        //public static string message = "";
+
+        public static void AddStudentToList(string name, string surname)
+        {
+            Student s = new Student();
+            s.Name = name;
+            s.Surname = surname;
+            s.RegisterDate = DateTime.Now;
+            studentList.Add(s);
+        }
+
+        public static void DeleteStudentFromList(Student std)
+        {
+            var index = studentList.FindIndex(w => w.Name == std.Name && w.Surname == std.Surname); // if condition is not matched, returns -1
+            if (index == -1)
+            {
+                isDeleted = false;
+            }
+            else
+            {
+                isDeleted = true;
+                studentList.RemoveAt(index);
+            }
+        }
+
+        public static void SortStudentList()
+        {
+            var sortedList = studentList.OrderByDescending(d => d.RegisterDate);
+            studentList = sortedList.ToList();
+        }
+    }
+}
