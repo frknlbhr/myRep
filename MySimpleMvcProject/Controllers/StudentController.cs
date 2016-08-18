6cfc1073c2d42ce7ffb1c97111d9c6d5e61e8f70
Furkan İlbahar <furkan_ilbahar@hotmail.com>
2016-08-18 05:55:50 +0300

    Some untracked files

---
+++ MySimpleMvcProject/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using MySimpleMvcProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySimpleMvcProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentList.isDeleted = false;
+            //TempData["Message"] = null;
+            ViewData["StdList"] = StudentList.studentList;
+
+            return View();
+        }
+
+        public ActionResult Add(Student studentModel)
+        {
+            StudentList.AddStudentToList(studentModel.Name, studentModel.Surname);
+            StudentList.SortStudentList();
+            ViewData["StdList"] = StudentList.studentList;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Student studentModel)
+        {
+            StudentList.DeleteStudentFromList(studentModel);
+            //TempData["Message"] = StudentList.message;
+            StudentList.SortStudentList();
+
+            ViewData["StdList"] = StudentList.studentList;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
